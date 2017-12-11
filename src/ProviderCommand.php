c8035aad012d50bb0465e4325d45cffb1cf943be
Pascal Birchler <hello@pascalbirchler.ch>
2017-12-11 18:58:08 +0100

    Add `wp embeds provider get` command

--- src/ProviderCommand.php
+++ src/ProviderCommand.php
@@ -6,6 +6,9 @@ use WP_CLI;
 use WP_CLI\Formatter;
 use WP_CLI_Command;
 
+/**
+ * Retrieves oEmbed providers.
+ */
 class ProviderCommand extends WP_CLI_Command {
 	protected $possible_fields = array(
 		'format',
@@ -47,7 +50,7 @@ class ProviderCommand extends WP_CLI_Command {
 	 * ## EXAMPLES
 	 *
 	 *     # List format,endpoint fields of available providers.
-	 *     $ wp oembed provider list --fields=format,endpoint
+	 *     $ wp embeds provider list --fields=format,endpoint
 	 *     +------------------------------+-----------------------------------------+
 	 *     | format                       | endpoint                                |
 	 *     +------------------------------+-----------------------------------------+
@@ -79,6 +82,82 @@ class ProviderCommand extends WP_CLI_Command {
 
 		$formatter = $this->get_formatter( $assoc_args );
 		$formatter->display_items( $providers );
+	}
+
+	/**
+	 * Get provider for a given URL.
+	 *
+	 * <url>
+	 * : URL to retrieve provider for.
+	 *
+	 * [--verbose]
+	 * : Show debug information.
+	 *
+	 * [--discover]
+	 * : Whether to use oEmbed discovery or not. Defaults to true.
+	 *
+	 * [--limit-response-size=<size>]
+	 * : Limit the size of the resulting HTML when using discovery. Default 150 KB.
+	 *
+	 * [--format=<format>]
+	 * : Which data format to prefer.
+	 * ---
+	 * default: json
+	 * options:
+	 *   - json
+	 *   - xml
+	 * ---
+	 *
+	 * ## EXAMPLES
+	 *
+	 *     # List format,endpoint fields of available providers.
+	 *     $ wp embeds provider get https://www.youtube.com/watch?v=dQw4w9WgXcQ
+	 *
+	 * @subcommand get
+	 */
+	public function get_provider( $args, $assoc_args ) {
+		$oembed = _wp_oembed_get_object();
+
+		$url                 = $args[0];
+		$discover            = \WP_CLI\Utils\get_flag_value( $assoc_args, 'discover', true );
+		$response_size_limit = \WP_CLI\Utils\get_flag_value( $assoc_args, 'limit-response-size' );
+		$format              = \WP_CLI\Utils\get_flag_value( $assoc_args, 'format' );
+
+		if ( $response_size_limit ) {
+			add_filter( 'oembed_remote_get_args', function ( $args ) use ( $response_size_limit ) {
+				$args['limit_response_size'] = $response_size_limit;
+
+				return $args;
+			} );
+		}
+
+		if ( $format ) {
+			add_filter( 'oembed_linktypes', function ( $linktypes ) use ( $format ) {
+				foreach ( $linktypes as $mime_type => $linktype_format ) {
+					if ( $format !== $linktype_format ) {
+						unset( $linktypes[ $mime_type ] );
+					}
+				}
+
+				return $linktypes;
+			} );
+		}
+
+		$oembed_args = array(
+			'discover' => $discover,
+		);
+
+		$provider = $oembed->get_provider( $url, $oembed_args );
+
+		if ( ! $provider ) {
+			if ( ! $discover ) {
+				WP_CLI::error( 'No oEmbed provider found for given URL.' );
+			} else {
+				WP_CLI::error( 'No oEmbed provider found for given URL. Maybe try discovery?' );
+			}
+		}
+
+		WP_CLI::success( sprintf('Provider found! URL: %s', $provider ) );
 	}
 
 	/**
