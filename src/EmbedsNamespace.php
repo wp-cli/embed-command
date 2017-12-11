52a85820eb2172c290be18bd67cc805d2e73469a
Pascal Birchler <hello@pascalbirchler.ch>
2017-12-11 18:32:45 +0100

    Use `wp scaffold package` for this command

---
+++ src/EmbedsNamespace.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace WP_CLI\Embeds;
+
+use WP_CLI\Dispatcher\CommandNamespace;
+
+/**
+ * Fetches and inspectes embeds
+ *
+ * @package wp-cli
+ */
+class EmbedsNamespace extends CommandNamespace {
+}
