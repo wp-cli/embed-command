83f0f1551afedebb31510039c155a00046bc5c2d
Pascal Birchler <hello@pascalbirchler.ch>
2017-12-22 11:51:35 +0100

    Fix docs for cache find command

--- src/Cache_Command.php
+++ src/Cache_Command.php
@@ -48,8 +48,8 @@ class Cache_Command extends WP_CLI_Command {
 	/**
 	 * Finds the oEmbed cache post ID for a given URL.
 	 *
-	 * Starts by checking the URL against the regex of the registered embed handlers.
-	 * If none of the regex matches and it's enabled, then the URL will be given to the WP_oEmbed class.
+	 * Starting with WordPress 4.9, embeds that aren't associated with a specific post will be cached in
+	 * a new oembed_cache post type.
 	 *
 	 * ## OPTIONS
 	 *
